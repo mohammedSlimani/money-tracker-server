abdfbe7419d84f5456c18589968e427093c95b75
Mohammed slimani <slimani.med.21@gmail.com>
2019-08-22 20:45:15 +0100

    injecting the Item validation into the User

--- src/user/index.js
+++ src/user/index.js
@@ -1,7 +1,8 @@
 import Id from '../Id';
 import validator from 'email-validator';
 import makeBuildUser from './user';
+import makeItem from '../item'
 
-const makeUser = makeBuildUser({ Id, validator })
+const makeUser = makeBuildUser({ Id, validator, makeItem })
 
 export default makeUser;
