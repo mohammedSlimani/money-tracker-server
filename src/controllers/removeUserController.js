eae7a00e38f02409f5635801b9f88485bda32263
Mohammed slimani <slimani.med.21@gmail.com>
2019-08-23 18:16:32 +0100

    controllers: Remove user

---
+++ src/controllers/removeUserController.js
@@ -0,0 +1,28 @@
+export default makeRemoveUserController({removeUser}){
+    return async function removeUserController(httpRequest){
+        const headers = {
+            'Content-Type': 'application/json'
+        }
+        try {
+            const id = httpRequest.params.id;
+            const user = await removeUser({ id });
+            return {
+                headers,
+                statusCode: 200,
+                body: {
+                    ...user
+                }
+            }
+
+        } catch (e) {
+            console.log(e);
+            return {
+                headers,
+                statusCode: 400,
+                body: {
+                    error: e.message
+                }
+            }
+        }
+    }
+}
