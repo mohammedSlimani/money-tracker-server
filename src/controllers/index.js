63c2486c1f2bcc2022c46facc38f2ae55d7c6a33
Mohammed slimani <slimani.med.21@gmail.com>
2019-08-23 18:31:43 +0100

    Injecting the UseCases to the Controllers and Exposing controllers

---
+++ src/controllers/index.js
@@ -0,0 +1,40 @@
+import {
+    addItem,
+    authUser,
+    editItem,
+    editUser,
+    removeItem,
+    removeUser
+} from '../use-cases'
+
+import makeAddItemController from './addItemController'
+import makeAuthUserController from './authUserController';
+import makeEditItemController from './editItemController';
+import makeRemoveItemController from './removeItemController';
+import makeEditUserController from './editUserController';
+import makeRemoveUserController from './removeUserController';
+
+const addItemController = makeAddItemController({addItem});
+const authUserController = makeAuthUserController({authUser});
+const editItemController = makeEditItemController({editItem});
+const removeItemController = makeRemoveItemController({removeItem});
+const editUserController = makeEditUserController({editUser});
+const removeUserController = makeRemoveUserController({removeUser});
+
+export default userControllers = Object.freeze({
+    addItemController,
+    authUserController,
+    editItemController,
+    editUserController,
+    removeItemController,
+    removeUserController
+})
+
+export {
+    addItemController,
+    authUserController,
+    editItemController,
+    editUserController,
+    removeItemController,
+    removeUserController
+}
