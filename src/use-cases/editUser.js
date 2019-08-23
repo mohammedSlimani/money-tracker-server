fbb05721cd4e8632f77a1ce41cb1a360bfbf2fd7
Mohammed slimani <slimani.med.21@gmail.com>
2019-08-23 14:06:33 +0100

    useCase: Edit User

---
+++ src/use-cases/editUser.js
@@ -0,0 +1,15 @@
+export default function makeEditUser({ usersDb }){
+    return async function editUser({ id, ...changes }){
+        if(!id){
+            throw new Error('Provide the Id of the user you want to edit');
+        }
+        
+        const exist = await usersDb.findById({ id });
+        if(!exist){
+            throw new Error('User to edit doesnt exist');
+        }
+        
+        const edited = await usersDb.update({id, ...changes});
+        return edited;
+    }
+}
