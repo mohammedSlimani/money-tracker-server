f26f4add1cb269dadc03519aa1dd4e7a4bfad6fa
Mohammed slimani <slimani.med.21@gmail.com>
2019-08-23 13:52:33 +0100

    Refactor

--- src/data-access/usersDb.js
+++ src/data-access/usersDb.js
@@ -1,6 +1,6 @@
 import Id from "../Id";
 
-export default function makeUserDb({ makeDb }){
+export default function makeUserDb({ makeDb }) {
     return Object.freeze({
         findAll,
         findById,
@@ -9,48 +9,48 @@ export default function makeUserDb({ makeDb }){
         remove,
         update
     })
-    async function findAll(){
+    async function findAll() {
         const db = await makeDb();
         const result = await db.collection('users').find({});
         return result;
     }
 
-    async function findById({id: _id}){
+    async function findById({ id: _id }) {
         const db = await makeDb();
-        const result = await db.collection('users').find({_id});
-        if(result.length === 0){
+        const result = await db.collection('users').find({ _id });
+        if (result.length === 0) {
             return null;
         }
         return result[0];
     }
 
-    async function findByEmail({ email }){
+    async function findByEmail({ email }) {
         const db = await makeDb();
-        const result = await db.collection('users').find({email});
-        if(result.length === 0){
+        const result = await db.collection('users').find({ email });
+        if (result.length === 0) {
             return null;
         }
         return result[0];
     }
 
-    async function insert({id:_id, ...userInfo}){
+    async function insert({ id: _id, ...userInfo }) {
         const db = await makeDb();
-        const result = await db.collection('users').insertOne({_id, ...userInfo})
+        const result = await db.collection('users').insertOne({ _id, ...userInfo })
         return result.ops[0];
     }
 
-    async function remove ({id:_id}){
+    async function remove({ id: _id }) {
         const db = await makeDb();
-        const result = await db.collection('users').deleteOne({_id});
+        const result = await db.collection('users').deleteOne({ _id });
         return result;
     }
 
-    async function update({id:_id, ...changes}){
+    async function update({ id: _id, ...changes }) {
         const db = await makeDb();
         const result = await db.collection('users').findOneAndUpdate(
-            {_id},
-            {$set:{...changes}},
-            {returnOriginal: false}
+            { _id },
+            { $set: { ...changes } },
+            { returnOriginal: false }
         )
         return result.value
     }
--- src/use-cases/addUser.js
+++ src/use-cases/addUser.js
@@ -1,25 +1,25 @@
 import makeUser from "../user";
 
-export default function makeAddUser({ usersDb }){
-    return async function addUser({name, facebookId, email} = {}){
-        const user = makeUser({name, facebookId, email});
-        const exist = usersDb.findByEmail({email});
+export default function makeAddUser({ usersDb }) {
+    return async function addUser({ name, facebookId, email } = {}) {
+        const user = makeUser({ name, facebookId, email });
+        const exist = usersDb.findByEmail({ email });
 
-        if(exist && facebookId === exist.facebookId){
+        if (exist && facebookId === exist.facebookId) {
             return exist
         }
-        else if (exist){
+        else if (exist) {
             throw new Error('FaceBookId dont match');
         }
 
-        //If we get here it is because there is on user with this email
+        //If we get here it is because there is no user with this email
         return usersDb.insert({
             id: user.getId(),
             name: user.getName(),
             email: user.getEmail(),
             facebookId: user.getFacebookId(),
             items: user.getItems()
-        }) 
+        })
 
     }
 }
