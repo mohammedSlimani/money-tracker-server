d3eb91b9a2aec2d9fedfdd0467a72d323ccdff84
Mohammed slimani <slimani.med.21@gmail.com>
2019-08-23 18:57:45 +0100

    exporting the makeDb for usage in the db index making

--- src/data-access/index.js
+++ src/data-access/index.js
@@ -6,7 +6,7 @@ const url = process.env.DB_URL || 'mongodb://127.0.0.1:27017';
 const dbName = process.env.DB_NAME || 'money';
 const client = new MongoClient(url, { useNewUrlParser: true });
 
-async function makeDb() {
+export async function makeDb() {
     if (!client.isConnected()) {
         await client.connect();
     }
