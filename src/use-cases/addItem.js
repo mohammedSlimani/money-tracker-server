da3ab265ab4cdd1f7202820035975ed077931afe
Mohammed slimani <slimani.med.21@gmail.com>
2019-08-25 19:02:13 +0100

    Refactor

--- src/use-cases/addItem.js
+++ src/use-cases/addItem.js
@@ -1,15 +1,22 @@
 import makeItem from "../item";
-import makeUser from "../user";
 
 export default function makeAddItem({ usersDb }){
     return async function addItem({id, item}){
+        if(!id){
+            throw new Error('You have to provide the Id of the user you want to add the item to')
+        }
         const mItem = makeItem( {title: item.title, price: item.price});
         const user = await usersDb.findById({id});
         if(!user){
             throw new Error('user to add item to doesnt exist')
         }
         return await usersDb.addItem({
-            item,
+            item:{
+                id: mItem.getId(),
+                title:mItem.getTitle(),
+                price:mItem.getPrice(),
+                dateAdded: mItem.getDateAdded()
+            },
             id 
         });
     }
