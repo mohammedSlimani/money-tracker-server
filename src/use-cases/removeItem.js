16586874a6c83ae65af2b8723715e6f7b2043bfb
Mohammed slimani <slimani.med.21@gmail.com>
2019-08-25 20:28:32 +0100

    Remove Item is Working Fine Now

--- src/use-cases/removeItem.js
+++ src/use-cases/removeItem.js
@@ -1,5 +1,12 @@
 export default function makeRemoveItem({ usersDb }){
-    return async function removeItem({ id, item }){
-        //TODO
+    return async function removeItem({ id, itemId }){
+        if(!id){
+            throw new Error('Give user Id to remove Item');
+        }
+        if(!itemId){
+            throw new Error('Give item Id to remove');
+        }
+        
+        return await usersDb.removeItem({userId: id, itemId})
     }
 }
